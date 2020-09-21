766fa2085a01a4601ad0d2e065c28f0380e8a2a2
weixuetao <wxtcss@163.com>
2020-09-21 22:00:51 +0800

    change HelloGit.java -- 添加了新内容

--- git-test/src/main/java/com/weitao/testGit/HelloGit.java
+++ git-test/src/main/java/com/weitao/testGit/HelloGit.java
@@ -3,8 +3,11 @@ package com.weitao.testGit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello clone git");
     }
     public void test(){
         System.out.println("test");
+        System.out.println("test-clone");
     }
+
 }
