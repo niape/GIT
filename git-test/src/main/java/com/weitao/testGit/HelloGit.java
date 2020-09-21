dc29e322fdb683ffbeda91084b05301f5283f192
weixuetao <wxtcss@163.com>
2020-09-21 21:43:40 +0800

    修改HelloGit.java  -- 添加了test方法

--- git-test/src/main/java/com/weitao/testGit/HelloGit.java
+++ git-test/src/main/java/com/weitao/testGit/HelloGit.java
@@ -4,4 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void test(){
+        System.out.println("test");
+    }
 }
